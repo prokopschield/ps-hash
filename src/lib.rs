5bee8e1f818e0ca446dc0b4eefb5184bf55669ad
Prokop <prokop.schield@protonmail.com>
2024-03-09 20:54:37 +0100

    chore(src/lib.rs): add some tests

--- src/lib.rs
+++ src/lib.rs
@@ -102,3 +102,33 @@ pub fn verify_hash_integrity(hash: &[u8]) -> bool {
 
     false
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    pub fn hash() {
+        let test_str = b"Hello, world!";
+        let test_value = test_str.as_slice();
+        let hash_value = super::hash(test_value);
+
+        assert_eq!(
+            "3Lqbann~vFOn43UpL64ukdU4TlKXU4nFFvUZCL1iFF5E1IlNDQ",
+            hash_value
+        );
+
+        let parts = super::decode_parts(hash_value.as_bytes()).unwrap();
+
+        assert_eq!(parts.2, test_value.len() as u16);
+    }
+
+    #[test]
+    pub fn hash_length() {
+        for input_length in 0..10000 {
+            let input = b"F".repeat(input_length);
+            let hash = super::hash(input.as_slice());
+            let (_, _, length) = super::decode_parts(hash.as_bytes()).unwrap();
+
+            assert_eq!(input_length % 4096, length as usize, "{}", input_length);
+        }
+    }
+}
