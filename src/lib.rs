66c501884e17ce1389b178cbb09866eb6a482375
Prokop <prokop.schield@protonmail.com>
2024-07-22 18:23:12 +0200

    refactor: separate out tests

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,9 @@ use ps_pint16::PackedInt;
 use sha2::{Digest, Sha256};
 use std::fmt::Write;
 
+#[cfg(test)]
+pub mod tests;
+
 pub const HASH_SIZE_BIN: usize = 32;
 pub const HASH_SIZE: usize = 50;
 
@@ -361,41 +364,4 @@ pub fn verify_hash_integrity(hash: &[u8]) -> bool {
     }
 
     false
-}
-
-#[cfg(test)]
-mod tests {
-    use ps_pint16::PackedInt;
-
-    #[test]
-    pub fn hash() {
-        let test_str = b"Hello, world!";
-        let test_value = test_str.as_slice();
-        let hash_value = super::hash(test_value).to_string();
-
-        assert_eq!(
-            "3Lqbann~vFOn43UpL64ukdU4TlKXU4nFFvUZCL1iFF5E1IlNDQ",
-            hash_value
-        );
-
-        let parts = super::decode_parts(hash_value.as_bytes()).unwrap();
-
-        assert_eq!(parts.2.to_usize(), test_value.len());
-    }
-
-    #[test]
-    pub fn hash_length() {
-        for input_length in 0..10000 {
-            let input = b"F".repeat(input_length);
-            let hash = super::hash(input.as_slice());
-            let (_, _, length) = super::decode_parts(hash.as_bytes()).unwrap();
-
-            assert_eq!(
-                PackedInt::from_usize(input_length),
-                length,
-                "{}",
-                input_length
-            );
-        }
-    }
 }
