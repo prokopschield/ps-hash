761550d27a073b0b92e17441dc04c6844335c009
Prokop <prokop.schield@protonmail.com>
2025-10-27 01:21:19 +0100

    refactor: remove PsHashError

--- src/error.rs
+++ src/error.rs
@@ -3,24 +3,6 @@
 use ps_ecc::{RSDecodeError, RSGenerateParityError};
 use thiserror::Error;
 
-#[derive(Error, Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub enum PsHashError {
-    #[error("The input was too short and could not be decoded.")]
-    InputTooShort,
-    #[error("Reading from a slice failed.")]
-    TryFromSliceError,
-    #[error("When converting &[u8] to Hash: incorrect length")]
-    BadInputLength,
-    #[error("When converting &[u8] to Hash: invalid byte")]
-    BadInputByte,
-}
-
-impl From<std::array::TryFromSliceError> for PsHashError {
-    fn from(_error: std::array::TryFromSliceError) -> Self {
-        Self::TryFromSliceError
-    }
-}
-
 #[derive(Clone, Debug, Error, PartialEq, Eq)]
 pub enum HashError {
     #[error(transparent)]
