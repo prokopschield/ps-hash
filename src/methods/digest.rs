b16c41230d5bc07ac0621e8756247d9510dd4186
Prokop <prokop.schield@protonmail.com>
2025-10-27 22:22:34 +0100

    refactor: rebase Hash::{digest,parity} on ps_util::subarray

--- src/methods/digest.rs
+++ src/methods/digest.rs
@@ -1,8 +1,10 @@
+use ps_util::subarray;
+
 use crate::{Hash, DIGEST_SIZE};
 
 impl Hash {
     #[must_use]
-    pub fn digest(&self) -> &[u8] {
-        &self.inner[..DIGEST_SIZE]
+    pub fn digest(&self) -> &[u8; DIGEST_SIZE] {
+        subarray(&self.inner, 0)
     }
 }
--- src/methods/parity.rs
+++ src/methods/parity.rs
@@ -1,8 +1,10 @@
-use crate::{Hash, PARITY_OFFSET};
+use ps_util::subarray;
+
+use crate::{Hash, PARITY_OFFSET, PARITY_SIZE};
 
 impl Hash {
     #[must_use]
-    pub fn parity(&self) -> &[u8] {
-        &self.inner[PARITY_OFFSET..]
+    pub fn parity(&self) -> &[u8; PARITY_SIZE] {
+        subarray(&self.inner, PARITY_OFFSET)
     }
 }
